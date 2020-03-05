c43fcc9a8f54ff84d55e412caee66faec91506d6
Emmett Crawford <emmettbcrawford@gmail.com>
2020-03-05 12:26:01 -0800

    Finished start menu

---
+++ President Game/Assets/mainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    public Scene loadScene;
+   public void newGame()
+    {
+
+        SceneManager.LoadScene(2);
+    }
+    public void loadGame()
+    {
+        // Add load game functionality 
+        // SceneManager.LoadScene(loadScene);
+        Debug.Log("LoadGame");
+    }
+}
